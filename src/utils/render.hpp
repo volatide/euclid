510d90ef2992f62850dea5bcaf03393c655ec2dc
Alve Svarén <alve@hotmail.se>
2021-04-07 22:50:36 +0200

    Rename functions

--- src/utils/render.hpp
+++ src/utils/render.hpp
@@ -9,9 +9,9 @@ namespace euclid::render {
 		Color(uint8_t red, uint8_t green, uint8_t blue);
 		Color(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha);
 
-		Color from_hsv(uint8_t hue, uint8_t saturation, uint8_t value);
-		Color from_hsv(uint8_t hue, uint8_t saturation, uint8_t value,
-					   uint8_t alpha);
+		Color fromHSV(uint8_t hue, uint8_t saturation, uint8_t value);
+		Color fromHSV(uint8_t hue, uint8_t saturation, uint8_t value,
+					  uint8_t alpha);
 	};
 
 	struct Point {
