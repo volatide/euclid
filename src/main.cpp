ba6ec9009bd7775cb54d976f3f762579fce7bb4f
E. Almqvist <elalmqvist@gmail.com>
2021-04-07 14:32:24 +0200

    Basic window

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,31 @@
+// Headers
 #include <iostream>
 
+// OpenGL headers
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
 int main() {
+	GLFWwindow* window;
+	
+	if(!glfwInit()) { return 1; } // if it fails to init then exit 1
+
+	window = glfwCreateWindow(640, 480, "Euclid Engine", NULL, NULL);
+	if(!window) {
+		glfwTerminate();
+		return 1;
+	}
+
+	glfwMakeContextCurrent(window); // make window current context
+
+	while(!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+
 	return 0;
 }
