573fd1e6746e97de4917abb41203027bd61cf77b
Alve Svarén <alve@hotmail.se>
2021-04-07 22:40:31 +0200

    Move color to render.hpp

--- src/utils/render.hpp
+++ src/utils/render.hpp
@@ -1,8 +1,18 @@
-#include "color.hpp"
 #include <glm/vec2.hpp> // glm::vec2
 #include <vector>
 
 namespace euclid::render {
+
+	struct Color {
+		uint8_t red, green, blue, alpha;
+
+		Color(uint8_t red, uint8_t green, uint8_t blue);
+		Color(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha);
+
+		Color from_hsv(uint8_t hue, uint8_t saturation, uint8_t value);
+		Color from_hsv(uint8_t hue, uint8_t saturation, uint8_t value,
+					   uint8_t alpha);
+	};
 
 	struct Point {
 		int x, y;
