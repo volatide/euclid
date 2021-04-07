84d15fe72e8b300aa34d714dce5ef4ed3f90c887
E. Almqvist <elalmqvist@gmail.com>
2021-04-07 23:36:25 +0200

    Cases

--- src/utils/opengl_render.cpp
+++ src/utils/opengl_render.cpp
@@ -4,6 +4,27 @@ namespace euclid::render {
 	using namespace glm;
 
 	void draw(std::vector<Point> points) {
-		
+		uint8_t numPoints = points.size();	
+		switch(numPoints) {
+			case 2: // line
+				glBegin(GL_LINES);
+				break;
+
+			case 3: // triangle
+				glBegin(GL_TRIANGLES);
+				break;
+
+			case 4: // quad
+				glBegin(GL_QUADS);
+				break;
+
+			default: // polygon
+				glBegin(GL_POLYGON);
+				break;
+		}
+
+		for(Point const& p : points) {
+			
+		}
 	}
 }
