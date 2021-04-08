5ec8f37232574507b8cfcd86d5ac68dc1898294e
E. Almqvist <elalmqvist@gmail.com>
2021-04-08 14:16:04 +0200

    Added logging main func

--- src/utils/logging.hpp
+++ src/utils/logging.hpp
@@ -7,6 +7,15 @@ namespace euclid {
 		APP
 	};
 
+	enum LogLevel {
+		DEBUG,
+		INFO,
+		WARN,
+		ERROR,
+		FATAL
+	};
+
+	void log(LogScope scope, const std::string message);
 	void debug(LogScope scope, const std::string message);
 	void info(LogScope scope, const std::string message);
 	void warn(LogScope scope, const std::string message);
@@ -19,3 +28,7 @@ namespace euclid {
 #define APP_DEBUG(message) euclid::debug(euclid::LogScope::APP, message);
 #define APP_INFO(message) euclid::info(euclid::LogScope::APP, message);
 #endif
+
+#define APP_WARN(message) euclid::warn(euclid::LogScope::APP, message);
+#define APP_ERROR(message) euclid::error(euclid::LogScope::APP, message);
+#define APP_FATAL(message) euclid::fatal(euclid::LogScope::APP, message);
