08f2a04ba0a35b8ae7d45ae54a906bbe8209446f
E. Almqvist <elalmqvist@gmail.com>
2021-04-08 14:19:38 +0200

    Removed macros

--- src/utils/logging.hpp
+++ src/utils/logging.hpp
@@ -14,21 +14,12 @@ namespace euclid {
 		ERROR,
 		FATAL
 	};
-
-	void log(LogScope scope, const std::string message);
+	
+	void log(LogScope scope, LogLevel level, const std::string message);
 	void debug(LogScope scope, const std::string message);
 	void info(LogScope scope, const std::string message);
 	void warn(LogScope scope, const std::string message);
 	void error(LogScope scope, const std::string message);
 	void fatal(LogScope scope, const std::string message);
-
 }
 
-#ifdef LOG_DEBUG
-#define APP_DEBUG(message) euclid::debug(euclid::LogScope::APP, message);
-#define APP_INFO(message) euclid::info(euclid::LogScope::APP, message);
-#endif
-
-#define APP_WARN(message) euclid::warn(euclid::LogScope::APP, message);
-#define APP_ERROR(message) euclid::error(euclid::LogScope::APP, message);
-#define APP_FATAL(message) euclid::fatal(euclid::LogScope::APP, message);
